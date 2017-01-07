5b5f90c2a9134414907a649ae4e7accfe5fe50bb
Matthew Sarantos <M-J-S@users.noreply.github.com>
2017-01-07 06:55:07 -0700

    Add files via upload

---
+++ TicTacToe.py
@@ -0,0 +1,18 @@
+# tic-tac-toe
+
+# Created board for players
+
+print("  |  |  ")
+print("  |  |  ")
+print("--+--+--")
+print("  |  |  ")
+print("  |  |  ")
+print("--+--+--")
+print("  |  |  ")
+print("  |  |  ")
+
+
+# need to make functions for win conditions and player 1 and 2 turns
+# need to make a 2d list so we can have a 3x3 matrix to match the board
+
+ 
