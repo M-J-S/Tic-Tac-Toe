1113576373981a31e79ec40cb1d9b82bd41b3def
Matthew Sarantos <M-J-S@users.noreply.github.com>
2017-01-10 05:07:20 -0700

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,18 +1,70 @@
 # tic-tac-toe
+TTT = {}
 
-# Created board for players
+TTT["TL"] = ' ' 
+TTT["TC"] = ' '
+TTT["TR"] = ' '
+TTT["ML"] = ' '
+TTT["MC"] = ' '
+TTT["MR"] = ' '
+TTT["BL"] = ' '
+TTT["BC"] = ' '
+TTT["BR"] = ' '
 
-print("  |  |  ")
-print("  |  |  ")
-print("--+--+--")
-print("  |  |  ")
-print("  |  |  ")
-print("--+--+--")
-print("  |  |  ")
-print("  |  |  ")
+
+computerScore = 0
+playerScore = 0
+player1Choice = ''
+player2Choice = ''
+global turn
+turn = True 
+
+def player1Turn(turn):
+    while turn == True:
+        player1Choice = input("")
+    
+        if(TTT[str(player1Choice)] == ' '):
+            TTT[str(player1Choice)] = 'X'
+            turn = False
+        else:
+            print("That space is taken")
+        
+    printBoard()
+
+    player2Turn(turn)
 
 
-# need to make functions for win conditions and player 1 and 2 turns
-# need to make a 2d list so we can have a 3x3 matrix to match the board
+def player2Turn(turn):
+    while turn == False:
+        player1Choice = input("")
+        
+        if(TTT[str(player1Choice)] == ' '):
+            TTT[str(player1Choice)] = 'O'
+            turn = True
+        else:
+            print("That space is taken")
+            
 
- 
+    printBoard()
+
+    player1Turn(turn)
+
+
+def printBoard():
+
+    print("    |    |   ")
+    print(" %s  | %s  | %s " % (TTT["TL"], TTT["TC"], TTT["TR"]))
+    print("----+----+----")
+    print(" %s  | %s  | %s  " % (TTT["ML"], TTT["MC"], TTT["MR"]))
+    print("----+----+----")
+    print(" %s  | %s  | %s  " % (TTT["BL"], TTT["BC"], TTT["BR"]))
+    print("    |    |   ")
+    
+while player1Choice != 'q':
+    player1Turn(turn)
+    #player2Turn(turn)    
+
+#def winCondition():
+    #break
+
+    # need to make functions for win conditions and player 1 and 2 turns
